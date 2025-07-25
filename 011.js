8cc6275fcf6001942ea981e428abc9eac3a2db05
Konstantsin <k4164724@gmail.com>
2025-07-25 11:07:42 +0300

    1

---
+++ 011.js
@@ -0,0 +1,11 @@
+let sum =0;// your code
+let i = 0;
+while (i<=20) {// С помощью цикла while найдите сумму чисел от 1 до 20
+    sum += i;
+    i++
+}
+console.log(sum)// Результат присвойте переменной sum
+
+
+// your code
+
